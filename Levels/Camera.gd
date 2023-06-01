9ae8776753456f4b9d76ab4301ed4b1a4303b99a
Lollo <41307169+LollosaurusRex@users.noreply.github.com>
2023-06-01 18:16:54 -0500

    Completed platformer tutorial, added camera zoom
    https://youtu.be/43c-Sm5GMbc

---
+++ Levels/Camera.gd
@@ -0,0 +1,24 @@
+extends Camera2D
+
+@export var minZoom : float = 1.0
+@export var maxZoom : float = 4.0
+@export var zoomDelta : float = 0.25
+
+var currZoom = 1
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_just_released("zoomin"):
+		currZoom += zoomDelta
+	if Input.is_action_just_released("zoomout"):
+		currZoom -= zoomDelta
+		
+	currZoom = clamp(currZoom, minZoom, maxZoom)
+	
+	zoom.x = currZoom
+	zoom.y = currZoom
+	
